57c0dc6df572bd11bc1d79d73c9f7cffc8c1d0f7
Eleutherado <emiliov432@gmail.com>
2017-04-06 03:39:47 -0400

    got middleware working, building wthrList

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import WeatherReducer from "./weather_reducer"
+import WeatherReducer from "./weather_reducer";
 
 const rootReducer = combineReducers({
-  Weather: WeatherReducer
+    weather: WeatherReducer
 });
 
 export default rootReducer;
--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,9 +1,11 @@
 import { FETCH_WEATHER } from "../actions/index";
 
-export default function (state = null, action) {
+export default function (state = [], action) {
+    console.log(action);
     switch(action.type) {
         case FETCH_WEATHER:
-            return [ action.payload.data, ...state ]; //
+            return state.concat([action.payload.data]); // contatenates two lists together
+            //state is flattened
     }
     return state;
 }
